3542bc10bdff51ece7c1edd4f017e510e4d85e13
Alya Ryzhuk <alinaryzhuk28@gmail.com>
2021-10-28 02:09:26 +0300

    test BP

---
+++ src/com/pb/ryzhuk/hm1/NADO.java
@@ -0,0 +1,24 @@
+package com.pb.ryzhuk.hm1;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NADO {
+    public static void main(String[] args) {
+        System.out.println("Введіть номер телефону в форматі: 380ХХХХХХХХХ");
+        Scanner scanner = new Scanner(System.in);
+        String phone = scanner.nextLine();
+        if (isPhoneNumberCorrect(phone) == true) {
+            String phoneNew = phone.replace("38", "9");
+            System.out.println("Номер в новому форматі " + phoneNew);
+        } else System.out.println("Номер введено в неправильному форматі");;
+    }
+    static boolean isPhoneNumberCorrect(String phone) {
+        Pattern pattern = Pattern.compile("(^(\\380))?(\\d{12})$");
+        Matcher matcher = pattern.matcher(phone);
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+}
